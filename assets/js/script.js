ffffa1b7f5ccc9d40c3144a5c12d07104c1f4303
Julien Sebag <julien.sebag@me.com>
2020-02-03 21:13:39 +0100

    Added modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,13 @@ function openModal(e) {
 	var element = e.parentNode;
 	element.classList.toggle("modal-open");
 }
+
+// Add background scroll nav
+window.onscroll = function () {
+	var nav = document.getElementById('nav');
+	if (window.pageYOffset > 300) {
+		nav.classList.add("nav-background");
+	} else {
+		nav.classList.remove("nav-background");
+	}
+}
